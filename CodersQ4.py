5088a6dcb0a4ca4f9c67bd0ccc7428f5b039ddc7
diogenes-dj <zyguifried@hotmail.com>
2023-05-22 17:25:51 -0300

    Ache o erro

---
+++ CodersQ4.py
@@ -0,0 +1,39 @@
+'''
+
+num1 = float(input("Digite um número a seguir: "))
+
+dobro = 2*num1
+
+print("O dobro do número digitado é:", dobro)
+
+
+cont = 0
+resultado = 0
+n = 1000
+
+while cont != n:
+
+    resultado = resultado + 1/(2**cont)
+
+    cont = cont + 1
+
+print(resultado)
+'''
+
+x = int
+i = int(0)
+t = int(0)
+e = int
+
+a = float(input("Digite um número: "))
+
+while i < x:
+    e = int(input("Digite um número: "))
+
+    t = t + e
+    i = i + 1
+
+print(i, a)
+
+
+
